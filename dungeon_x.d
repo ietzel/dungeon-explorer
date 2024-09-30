386fb9e7aab17e5a0252ff13e8157bd7243efc86
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-30 19:06:03 -0400

    Update dungeon_x.d

--- dungeon_x.d
+++ dungeon_x.d
@@ -1,5 +1,5 @@
 import std.stdio;
 
 void main() {
-    writeln("Aw, it was so bad we had to censor the whole screen!!! Some showgirls and showboys are noticed nevertheless.");
+    writeln("Aw, it was so bad we had to censor the whole screen!!! Some showgirls and showboys are noticed loligagging nevertheless.");
 }
