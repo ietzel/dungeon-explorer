01cc7e534db679846e4be3bb3601ba682a3881de
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-01 15:50:07 -0400

    Update dungeon_x.d

--- dungeon_x.d
+++ dungeon_x.d
@@ -1,5 +1,5 @@
 import std.stdio;
 
 void main() {
-    writeln("Aw, it was so bad we had to censor the whole screen!!! A smorgasbord of showgirls and showboys are noticed loligagging nevertheless; one of them starts playing IceJJFish's On the Floor on their smartwatch, while a person interrupts that they're still showering and trying to listen to Lil B's RED FLAME mixtape.");
+    writeln("Aw, it was so bad we had to censor the whole screen!!! A smorgasbord of showgirls and showboys are noticed loligagging nevertheless; one of them starts playing IceJJFish's On the Floor on their smartwatch, while a person interrupts that they're still showering and trying to listen to Lil B's RED FLAME mixtape. One of them screams as a robotic centipede with red eyes and large sharp mettalic antennae slithers past.");
 }
