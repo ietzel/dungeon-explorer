43b9057d44d35df125d4c94c75d3623e03665e05
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-01 16:02:43 -0400

    Update dungeon_x.d

--- dungeon_x.d
+++ dungeon_x.d
@@ -1,5 +1,5 @@
 import std.stdio;
 
 void main() {
-    writeln("Aw, it was so bad we had to censor the whole screen!!! A smorgasbord of showgirls and showboys are noticed loligagging nevertheless; one of them starts playing IceJJFish's On the Floor on their smartwatch, while a person interrupts that they're still showering and trying to listen to Lil B's RED FLAME mixtape. One of them screams as a robotic centipede with red eyes and large sharp mettalic antennae slithers past. A door opens showcasing people dressed in white and assorted bling, as they march in carrying an artifact of sorts. A black backgrounded computer beeps revealing green text instructing people.");
+    writeln("Aw, it was so bad we had to censor the whole screen!!! A smorgasbord of showgirls and showboys are noticed loligagging nevertheless; one of them starts playing IceJJFish's On the Floor on their smartwatch, while a person interrupts that they're still showering and trying to listen to Lil B's RED FLAME mixtape. One of them screams as a robotic centipede with red eyes and large sharp mettalic antennae slithers past. A door opens showcasing people dressed in white and assorted bling, as they march in carrying an artifact of sorts. A black backgrounded computer beeps revealing green text instructing people. A lifeguard reading it runs to the shower for an allegedly drowned.");
 }
