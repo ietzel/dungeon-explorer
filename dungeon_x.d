74ee8f4e93d1259c725811aecc502f5be41e3a63
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-01 14:31:17 -0400

    Update dungeon_x.d

--- dungeon_x.d
+++ dungeon_x.d
@@ -1,5 +1,5 @@
 import std.stdio;
 
 void main() {
-    writeln("Aw, it was so bad we had to censor the whole screen!!! A smorgasbord of showgirls and showboys are noticed loligagging nevertheless.");
+    writeln("Aw, it was so bad we had to censor the whole screen!!! A smorgasbord of showgirls and showboys are noticed loligagging nevertheless; one of them starts playing IceJJFish's On the Floor on their smartwatch, while a person interrupts that they're still showering and trying to listen to Lil B's RED FLAME mixtape.");
 }
