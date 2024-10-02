7883a75d6df8f0310a42cf0af714eee2e516e0ac
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-02 13:02:31 -0400

    Update dungeon_x.d

--- dungeon_x.d
+++ dungeon_x.d
@@ -1,5 +1,11 @@
 import std.stdio;
 
 void main() {
-    writeln("Aw, it was so bad we had to censor the whole screen!!! A smorgasbord of showgirls and showboys are noticed loligagging nevertheless; one of them starts playing IceJJFish's On the Floor on their smartwatch, while a person interrupts that they're still showering and trying to listen to Lil B's RED FLAME mixtape. One of them screams as a robotic centipede with red eyes and large sharp mettalic antennae slithers past. A door opens showcasing people dressed in white and assorted bling, as they march in carrying an artifact of sorts. A black backgrounded computer beeps revealing green text instructing people. A lifeguard reading it runs to the shower for an allegedly drowned; a sumo wrestler takes the place proclaiming to wait until they have done finishing the section.");
+    writeln("Aw, it was so bad we had to censor the whole screen!!!");
+    writeln("A smorgasbord of showgirls and showboys are noticed loligagging nevertheless; one of them starts playing IceJJFish's On the Floor on their smartwatch, while a person interrupts that they're still showering and trying to listen to Lil B's RED FLAME mixtape.");
+    writeln("One of them screams as a robotic centipede with red eyes and large sharp mettalic antennae slithers past.");
+    writeln("A door opens showcasing people dressed in white and assorted bling, as they march in carrying an artifact of sorts.");
+    writeln("A black backgrounded computer beeps revealing green text instructing people.
+    writeln("A lifeguard reading it runs to the shower for an allegedly drowned; a sumo wrestler takes the place proclaiming to wait until they have done finishing the section.");
+    writeln("Randomly someone exclaims oh my goodness that thing is so cute.");
 }
